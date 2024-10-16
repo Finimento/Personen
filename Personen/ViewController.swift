e6fb33db230eb4993522c5dfa33e96e45bf42cd5
fiesematente <mosesfinimento@googlemail.com>
2024-10-16 03:38:37 +0200

    Initial Commit

---
+++ Personen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personen
+//
+//  Created by Mike Finimento on 16.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
